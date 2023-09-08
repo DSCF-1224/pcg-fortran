dc318b8528adbf93ca2adfff63f009d3a133b799
DSCF-1224 <take36fcp@vesta.ocn.ne.jp>
2023-09-09 07:59:55 +0900

    Add a implementation: `pcg_output_xsh_rs_16_8` - Added the implementation: `pcg_output_xsh_rs_16_8` - Added the test for this `FUNCTION` - Updated the files for compilation

--- src/pcg_fortran.f90
+++ src/pcg_fortran.f90
@@ -5,5 +5,34 @@ module pcg_fortran
     implicit none
 
     private
+    public  :: test_pcg_output_xsh_rs
+
+
+
+    interface pcg_output_xsh_rs
+
+        !> Output function: XHS RS
+        module pure elemental function pcg_output_xsh_rs_16_8(state) result(xsh_rs)
+
+            !> A dummy argument for this FUNCTION
+            integer(int16), intent(in) :: state
+
+            !> The return value of this FUNCTION
+            integer(int8) :: xsh_rs
+
+        end function pcg_output_xsh_rs_16_8
+
+    end interface pcg_output_xsh_rs
+
+
+
+    interface
+
+        !> A SUBROUTINE for a test: `pcg_output_xsh_rs`
+        module subroutine test_pcg_output_xsh_rs
+        ! There is no dummy arugment for this SUBROUTINE
+        end subroutine test_pcg_output_xsh_rs
+
+    end interface
 
 end module pcg_fortran
