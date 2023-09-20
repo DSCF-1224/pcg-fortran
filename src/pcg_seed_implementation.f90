ebac261d7fe3588c4c3e93788111c8f5c7392d66
DSCF-1224 <take36fcp@vesta.ocn.ne.jp>
2023-09-21 06:53:34 +0900

    Update a test program: `pcg_*_*_srandom_r` Changed the position to execute the tests.

--- src/pcg_seed_implementation.f90
+++ src/pcg_seed_implementation.f90
@@ -45,10 +45,7 @@ submodule (pcg_fortran) pcg_seed_implementation
 
 
 
-    subroutine test_pcg_seed_core(rng)
-
-        !> A dummy argument for this SUBROUTINE
-        class(pcg_state_type), intent(inout) :: rng
+    module procedure test_pcg_seed
 
         !> A local variable for this SUBROUTINE
         !> The device number to output the result of this test
@@ -296,89 +293,7 @@ submodule (pcg_fortran) pcg_seed_implementation
 
 
         ! close the used file
-        close(write_unit)       
-
-    end subroutine test_pcg_seed_core
-
-
-
-    module procedure test_pcg_seed
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_mcg_8_type) :: rng_mcg_8
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_mcg_16_type) :: rng_mcg_16
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_mcg_32_type) :: rng_mcg_32
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_mcg_64_type) :: rng_mcg_64
-
-
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_oneseq_8_type) :: rng_oneseq_8
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_oneseq_16_type) :: rng_oneseq_16
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_oneseq_32_type) :: rng_oneseq_32
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_oneseq_64_type) :: rng_oneseq_64
-
-
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_setseq_8_type) :: rng_setseq_8
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_setseq_16_type) :: rng_setseq_16
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_setseq_32_type) :: rng_setseq_32
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_setseq_64_type) :: rng_setseq_64
-
-
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_unique_8_type) :: rng_unique_8
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_unique_16_type) :: rng_unique_16
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_unique_32_type) :: rng_unique_32
-
-        !> A local variable for this SUBROUTINE
-        type(pcg_state_unique_64_type) :: rng_unique_64
-
-
-
-        call test_pcg_seed_core(rng_mcg_8 )
-        call test_pcg_seed_core(rng_mcg_16)
-        call test_pcg_seed_core(rng_mcg_32)
-        call test_pcg_seed_core(rng_mcg_64)
-
-        call test_pcg_seed_core(rng_oneseq_8 )
-        call test_pcg_seed_core(rng_oneseq_16)
-        call test_pcg_seed_core(rng_oneseq_32)
-        call test_pcg_seed_core(rng_oneseq_64)
-
-        call test_pcg_seed_core(rng_setseq_8 )
-        call test_pcg_seed_core(rng_setseq_16)
-        call test_pcg_seed_core(rng_setseq_32)
-        call test_pcg_seed_core(rng_setseq_64)
-
-        call test_pcg_seed_core(rng_unique_8 )
-        call test_pcg_seed_core(rng_unique_16)
-        call test_pcg_seed_core(rng_unique_32)
-        call test_pcg_seed_core(rng_unique_64)
+        close(write_unit)
 
     end procedure test_pcg_seed
 
