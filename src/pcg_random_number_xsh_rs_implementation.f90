6c505ddd4f5be23131bfd1f57a8a5a1d1742238b
DSCF-1224 <take36fcp@vesta.ocn.ne.jp>
2023-09-29 06:47:13 +0900

    Add a implementation: `pcg_*_32_xsh_rs_16_random_r` - Added the implementation - Updated the files for compilation - Updated the test for this `SUBROUTINE`

--- src/pcg_random_number_xsh_rs_implementation.f90
+++ src/pcg_random_number_xsh_rs_implementation.f90
@@ -16,4 +16,17 @@ submodule (pcg_fortran) pcg_random_number_xsh_rr_implementation
 
     end procedure pcg_random_number_16_xsh_rs_8
 
+
+
+    module procedure pcg_random_number_32_xsh_rs_16
+
+        !> A local variable for this SUBROUTINE
+        integer(int32) :: old_state
+
+        old_state = rng%state
+        call rng%step()
+        harvest = pcg_output_xsh_rs(old_state)
+
+    end procedure pcg_random_number_32_xsh_rs_16
+
 end submodule pcg_random_number_xsh_rr_implementation
