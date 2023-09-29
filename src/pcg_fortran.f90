7620268cc01497c5be4a78ae4ca1054340c51a2d
DSCF-1224 <take36fcp@vesta.ocn.ne.jp>
2023-09-30 08:24:46 +0900

    Add a implementation: `pcg_*_64_xsl_rr_rr_64_random_r` - Added the implementation - Updated the files for compilation - Added the test for this `SUBROUTINE`

--- src/pcg_fortran.f90
+++ src/pcg_fortran.f90
@@ -156,6 +156,7 @@ module pcg_fortran
         procedure, nopass, private :: pcg_multiply_default_multiplier_64
 
         procedure, pass(rng), private :: pcg_step_for_seeding_64
+        procedure, pass(rng), private :: pcg_random_number_64_xsl_rr_rr_64
 
         procedure(pcg_advance_64_abstract), pass(rng), private, deferred :: advance
 
@@ -414,6 +415,8 @@ module pcg_fortran
         procedure, pass(rng), public  :: seed       => pcg_seed_64_oneseq
         procedure, pass(rng), private :: step       => pcg_step_64_oneseq
 
+        generic, public :: random_number_xsl_rr_rr => pcg_random_number_64_xsl_rr_rr_64
+
     end type pcg_state_oneseq_64_type
 
 
@@ -433,6 +436,8 @@ module pcg_fortran
 
         generic, public :: seed => pcg_seed_64_setseq
 
+        generic, public :: random_number_xsl_rr_rr => pcg_random_number_64_xsl_rr_rr_64
+
     end type pcg_state_setseq_64_type
 
 
@@ -446,6 +451,8 @@ module pcg_fortran
         procedure, pass(rng), public  :: initialize => pcg_initialize_unique_64
         procedure, pass(rng), public  :: seed       => pcg_seed_64_unique
         procedure, pass(rng), private :: step       => pcg_step_64_unique
+
+        generic, public :: random_number_xsl_rr_rr => pcg_random_number_64_xsl_rr_rr_64
 
     end type pcg_state_unique_64_type
 
@@ -957,6 +964,25 @@ module pcg_fortran
             class(pcg_state_unique_64_type), intent(out) :: rng
 
         end subroutine pcg_initialize_unique_64
+
+    end interface
+
+
+
+    interface
+
+        !> `pcg_oneseq_64_xsl_rr_rr_64_random_r`
+        !> `pcg_unique_64_xsl_rr_rr_64_random_r`
+        !> `pcg_setseq_64_xsl_rr_rr_64_random_r`
+        module subroutine pcg_random_number_64_xsl_rr_rr_64(rng, harvest)
+
+            !> A dummy argument for this SUBROUTINE
+            class(pcg_state_64_type), intent(inout) :: rng
+
+            !> A dummy argument for this SUBROUTINE
+            integer(int64), intent(out) :: harvest
+
+        end subroutine pcg_random_number_64_xsl_rr_rr_64
 
     end interface
 
